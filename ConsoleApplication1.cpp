45486159fddb10ca7ce2b9cfa0c2f5e56f369f95
lewykin1996 <lewykin1996@gmail.com>
2025-12-09 19:20:59 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Дмитрий\n";
+	std::cout << 29 << "\n";
+}
+
